c375d508649e8365e3da4f077ba31e00f079f8ee
sssuryaaa <suryaprakash2912@gmail.com>
2025-11-12 21:20:05 +0530

    3rd commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,70 @@
+import { useState } from "react";
+
 const Header = () => {
+
+    const [openMovieDropdown, setOpenMovieDropdown] = useState(false);
+    const [openTVDropdown, setOpenTVDropdown]= useState(false);
+    const [openPeopleDropdown, setOpenPeopleDropdown] = useState(false);
+
     return (
-        <div className="header flex justify-between m-2 mt-3.5">
+        <div className="header flex justify-between mb-2 bg-[#003153] relative shadow-[0_4px_4px_-2px_rgba(0,0,0,0.2)] p-4 text-white">
             <h1 className="logo">Logo</h1>
             <ul className="header-list flex">
                 <li className="mx-8">Home</li>
-                <li className="mx-8">Movies</li>
-                <li className="mx-8">TV Shows</li>
-                <li className="mx-8">People</li>
+                <li className="mx-8" onMouseEnter={() => setOpenMovieDropdown(true)} onMouseLeave={()=> setOpenMovieDropdown(false)}>
+                    Movies
+                    {
+                        openMovieDropdown && (
+                        <div className="movie-dropdown absolute text-black">
+                            <div>
+                                Popular
+                            </div>
+                            <div>
+                                Now Playing
+                            </div>
+                            <div>
+                                Upcoming
+                            </div>
+                            <div>
+                                Top Rated
+                            </div>
+                        </div>
+                        )
+                    }                   
+                </li>
+                <li className="mx-8" onMouseEnter={()=> setOpenTVDropdown(true)} onMouseLeave={() => setOpenTVDropdown(false)}>
+                    TV Shows
+                    {
+                        openTVDropdown && (
+                            <div className="tv-dropdown absolute bg-white shadow-xl text-black">
+                                <div className="hover:bg-gray-200 p-1 px-1.5">
+                                    popular
+                                </div>
+                                <div className="hover:bg-gray-200 p-1 px-1.5">
+                                    Airing Today
+                                </div>
+                                <div className="hover:bg-gray-200 p-1 px-1.5">
+                                    On the Air
+                                </div>
+                                <div className="hover:bg-gray-200 p-1 px-1.5">
+                                    Top Rated
+                                </div>
+                            </div>
+                        )
+                    }
+                </li>
+                <li className="mx-8" onMouseEnter={()=>setOpenPeopleDropdown(true)} onMouseLeave={() => setOpenPeopleDropdown(false)}>
+                    People
+                    {
+                        openPeopleDropdown && (
+                            <div className="people-dropdown absolute text-black">
+                                <div>
+                                    popular
+                                </div>
+                            </div>
+                        )
+                    }
+                </li>
             </ul>
         </div>
     )
--- src/components/TVCard.js
+++ src/components/TVCard.js
@@ -7,7 +7,7 @@ const TVCard = () => {
             <ul>
                 <Link to="/tv/popular" target="_blank"><li>Popular</li></Link>
                 <Link to="/tv/airing_today" target="_blank"><li>Airing Today</li></Link>
-                <Link to="/tv/on_the_air" target="_blank"><li>On TV</li></Link>
+                <Link to="/tv/on_the_air" target="_blank"><li>On the Air</li></Link>
                 <Link to="/tv/top_rated" target="_blank"><li>Top Rated</li></Link>
             </ul>
         </div>
